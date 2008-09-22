4a70f5c22b667436167de6c1be5a70b4fd3062cf
Rudy A. Sombillo <rsombillo@gmail.com>
2008-09-22 12:02:30 -0700

    Changed the way NetNumber handles bad phone numbers.

--- lib/netnumber.rb
+++ lib/netnumber.rb
@@ -1,35 +1,38 @@
+require 'dnsruby'
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 class Netnumber
+
+  # hash of Tapioca Mobile supported wireless carriers, mapping the NNIDs to the carrier IDs
+  NNID_TO_CARRIER_ID = { 
+    "100313" => 1, # Verizon Wireless
+    "100321" => 2, # AT&T Wireless
+    "100335" => 5, # T-Mobile USA
+    "100343" => 3  # Sprint Spectrum
+  }
   
-  NNID_TO_CARRIER_ID = { "100313" => 1, "100321" => 2, "100335" => 5, "100343" => 3}
   WIRELESS_NNIDS = NNID_TO_CARRIER_ID.keys
   
-  attr_reader :nnid, :response
+  attr_reader :nnid, :response, :error_message
   
   def initialize(mobile_phone)
     @mobile_phone = mobile_phone
     @response = query_netnumber_service(@mobile_phone)
   end
   
-  def self.resolve(mobile_phone)
-    new(mobile_phone)
-  end
-  
   def carrier_id
     @nnid and NNID_TO_CARRIER_ID[@nnid]
   end
   
-  # used to query the NetNumber service edges, will put in here for now, but maybe there is a better place for it?
-  # returns an answer if the query was successful
-  # otherwise, returns a specific code to denote what type of error occurred 
+  # query the NetNumber service
   def query_netnumber_service(mobile_phone)
-    # FIXME the services array probably shouldn't be here
     services = ["65.214.42.88", "65.216.77.208"]
+    #services = ["0.0.0.0", "0.0.0.0"]
+    
+    # transform the phone number into reverse dot notation format
     mobile_phone = "1" << mobile_phone unless mobile_phone[0,1] == '1'
     mobile_phone.reverse!
-    
     if mobile_phone.length == 11
   	  index = 1
   	  for k in 1..11
@@ -43,16 +46,16 @@ class Netnumber
 	  for i in 1..services.length	      
   	  begin
   	    resolver = Dnsruby::Resolver.new(:nameserver => services[current_index], :query_timeout => 15)
-  	    response = resolver.query(mobile_phone,'ANY')
-  	    @nnid = decode_nnid_from_answer(response)
+  	    @response = resolver.query(mobile_phone,'ANY')
+  	    @nnid = decode_nnid_from_answer(@response)
   	    break
+  	  # if the phone number format was incorrect, a NXDomain exception is thrown by dnsruby
   	  rescue Dnsruby::NXDomain
   	    @response = 0
   	    break
-  	  rescue Dnsruby::ResolvError
-  	    @response = -1
-  	  rescue Dnsruby::ResolvTimeout
-  	    @response = -1
+  	  # dnsruby is unable to query NetNumber due to a timeout and/or service failure
+  	  rescue Dnsruby::ResolvError, Dnsruby::ResolvTimeout
+  	    @error_message = "We are unable to determine your carrier at this time. Please select your carrier from the list below."
   	  end
 	    
 	    if current_index == services.size - 1
@@ -60,20 +63,22 @@ class Netnumber
 	    else
 	      current_index += 1
 	    end  
-    end # end for-looping thru services array
+    end # end looping thru services array
+    
     resolver.close
     return @response   	  
   end
   
   # checks if this company is one of the wireless carriers
-  def is_wireless?
+  def valid?
     WIRELESS_NNIDS.include? nnid
   end
   
   private
-    # not sure if this is really needed, maybe put in query_netnumber_service
+    # once a valid response is received from NetNumber, we need to pull just the six digit NNID from the response
     def decode_nnid_from_answer(response)
-      nnid = ""; response.each_answer {|answer| nnid << answer.rdata_to_string}
+      nnid = ""
+      response.each_answer {|answer| nnid << answer.rdata_to_string}
       readable_nnid = ""
       for j in 8..nnid.length
         readable_nnid << nnid[j,1] if j % 2 == 0
